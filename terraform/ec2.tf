9aaf59019db413ccc3036b1101ec970024fd5d3f
Samylle Sales <37720085+samyev@users.noreply.github.com>
2021-10-24 19:17:31 -0300

    Update ec2.tf

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -11,11 +11,10 @@ data "aws_ami" "ubuntu" {
 
 resource "aws_instance" "app_server" {
   ami                    = data.aws_ami.ubuntu.image_id
-  instance_type          = var.instance_type
+  instance_type          = "t2.micro"
   vpc_security_group_ids = [aws_security_group.allow_http.id, aws_security_group.allow_ssh.id]
 
   tags = {
     Name = var.instance_name
-    CreatedBy = "Terraform"
   }
 }
